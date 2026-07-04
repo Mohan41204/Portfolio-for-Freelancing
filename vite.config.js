ca671830e6f065b91395a578de21f7ea5385d7b3
Mohan Kumar <mohan41204@gmail.com>
2026-07-04 15:32:41 +0530

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,28 @@
+// import path from "node:path";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// });
+
+
 import path from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/Portfolio-for-Freelancing/",
+
   plugins: [react(), tailwindcss()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
